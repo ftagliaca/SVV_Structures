2a64537d9976e2f63c2b367dc65004ff3d6bdc45
Filippo <filippotagliacarne@gmail.com>
2020-02-20 15:14:00 +0100

    Added Von Mises stress function

--- internalLoadsStresses.py
+++ internalLoadsStresses.py
@@ -1,3 +1,5 @@
+import math
+
 def normalStress(y, z, Aileron, M_z, M_x):
     '''
     Input:
@@ -14,3 +16,18 @@ def normalStress(y, z, Aileron, M_z, M_x):
     '''
     sigma_z = (M_z*Aileron.Iyy*y + M_x*Aileron.Izz*z)/(Aileron.Izz*Aileron.Iyy)
     return sigma_z
+
+def vonMises(sigma, tau):
+    '''
+    Input:
+
+    sigma = list with len = 3 containing [sigma_xx, sigma_yy, sigma_zz]
+    tau = list with len = 3 containing [tau_xy, tau_xz, tau_yz]
+
+    Output:
+
+    sigma_vm = Von Mises stress
+    '''
+    sigma_vm = math.sqrt(((sigma[0]-sigma[1])**2 + (sigma[1]-sigma[2])**2 + (sigma[0]-sigma[2])**2)/2\
+    3*(tau[0]**2+tau[1]**2+tau[2]**2))
+    return sigma_vm
