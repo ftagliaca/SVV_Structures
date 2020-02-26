aero_laods_test: Fixed average calculation, added test cases.

aero_loads_test.py
@@ -9,32 +9,31 @@ import numpy as np
 import unittest
 import random
 
-# Using 0.011 and 0.11
-# Test 1:
-# average diff: 9.320488189361016e-08
-# average diff: 6.095636171884944e-07
-# average diff: 8.289327858409918e-07
-# average diff: 1.4716753667975696e-06
-# average diff: -5.028727649139186e-07
-# .Test 2:
-# average diff: -1.4372062435501226e-06
-# average diff: 6.712648547564665e-08
-# average diff: -1.4372062435501226e-06
-# average diff: 1.3260976223282184e-06
-# average diff: 2.0125387562366698e-07
 
 class AeroLoadsTest(TestCase):
 
-    def generic_test_aero_loads(self, f: int, label: str = 'Aerodynamic Loading'):
+    def __init__(self, methodName='runTest'):
+        super().__init__(methodName=methodName)
+        self.f1 = lambda x1, x2: np.sin(x1 * x2)
+        self.f2 = lambda x1, x2: np.sin((x1 - 1) * 3.2) * np.sin((x2 - 7) * 0.3)
+        self.f3 = lambda x1, x2: np.sin((x1 - 0.5) * 3)**2 + np.cos((x2 - 0.5) * 0.14)**2
+
+        self.fs = [self.f1, self.f2, self.f3]
+
+
+    def generic_test_aero_loads(self, f: int, label: str = 'Aerodynamic Loading', grid: Tuple = (np.arange(-0.5, 0.5, 0.011), np.arange(-7, 7, 0.11), np.arange(-0.5 + 0.02, 0.5, 0.01), np.arange(-7 + 0.1, 7, 0.04))):
         def f_(x: np.ndarray, y: np.ndarray):
             return f(x[:, np.newaxis], y[np.newaxis, :])
 
         # Source data for interpolation
         
-        x = np.arange(-0.5, 0.5, 0.011)
-        y = np.arange(-7, 7, 0.11)
+        x, y, interp_x, interp_y = grid
+
         data_x, data_y = np.meshgrid(x, y, indexing='ij')
         data = f_(x, y)
+
+        scaling_factor = data.max() - data.min()
+        data /= scaling_factor
 
         def z_i(i, N_z = -1):
             return x[i - 1]
@@ -45,9 +44,7 @@ class AeroLoadsTest(TestCase):
         A320.z_i, A320.x_i = z_i, x_i
 
         # Target and verification data
-        interp_x = np.arange(-0.5 + 0.02, 0.5, 0.01)
-        interp_y = np.arange(-7 + 0.1, 7, 0.04)
-        data_verification = f_(interp_x, interp_y)
+        data_verification = f_(interp_x, interp_y) / scaling_factor
         data_interpolation = data_verification * 0
         mesh_x, mesh_y = np.meshgrid(interp_x, interp_y, indexing='ij')
 
@@ -58,23 +55,18 @@ class AeroLoadsTest(TestCase):
 
             aero_loads = AerodynamicLoad(A320, file, correction_factor=1)
         
-        data_interpolation = aero_loads
+        data_interpolation = aero_loads.get_values_grid(interp_x, interp_y)
 
-        for x_i, x in enumerate(interp_x):
-            for y_i, y in enumerate(interp_y):
-                data_interpolation[x_i, y_i] = aero_loads.get_value_at(x, y)
-        
-        error = np.abs(np.average(data_verification - data_interpolation))
+        error = np.average(np.abs(data_verification - data_interpolation))
         print(f"average error: {error}")
 
         fig = plt.figure(label)
         ax = plt.axes(projection='3d')
         
 
-#        ax.plot_wireframe(mesh_x, mesh_y, data_verification, color='black', label="Verification data")
-        ax.plot_wireframe(mesh_x, mesh_y, data_interpolation, color='green', label=f"Interpolated data (Avg Error: {error})")
-        ax.plot_wireframe(data_x, data_y, data, color='red', label="Source data")
-
+        ax.plot_wireframe(mesh_x, mesh_y, data_verification, color='black', label="Verification data")
+        ax.plot_wireframe(mesh_x, mesh_y, data_interpolation, color='blue', label=f"Interpolated data (Avg Error: {error})")
+#        ax.plot_wireframe(data_x, data_y, data, color='green', label="Source data")
 
         ax.set_xlabel("z")
         ax.set_ylabel("x")
@@ -82,26 +74,50 @@ class AeroLoadsTest(TestCase):
 
         plt.legend()
 
-        fig.show()
+        #fig.show()
 
-        self.assertAlmostEqual(np.average(data_verification - data_interpolation), 0, 3)
+        self.assertAlmostEqual(np.average(data_verification - data_interpolation), 0, 2)
 
-    def test_1(self):
-        print("Test 1:")
-        for i, _ in enumerate(range(5)):
-            r = random.random()
-            f = lambda x1, x2: np.sin(x1 * x2 * 0.25 * r)
-            self.generic_test_aero_loads(f, label=f'Test 1.{i+1}, r = {r}')
 
-    def test_2(self):
-        print("Test 2:")
-        for i, _ in enumerate(range(5)):
-            r = random.random()
-            r1 = 1 if random.random() >= 0.5 else r
-            r2 = 1 if random.random() < 0.5 else r
+    def run_tests(self, grid: Tuple, label = "Generic Grid"):
+        for i, f in enumerate(self.fs):
+            self.generic_test_aero_loads(f, label=f'Test {i} ({label})', grid=grid)
 
-            f = lambda x1, x2: np.sin(x1 * r2) * np.sin(x2 * r1)
-            self.generic_test_aero_loads(f, label=f'Test 2.{i+1}, r = {r}')
+
+    def test_grid_1(self):
+        print("Test grid 1:")
+        x = np.arange(-0.5, 0.5, 0.011)
+        y = np.arange(-7, 7, 0.11)
+        interp_x = np.arange(-0.5 + 0.02, 0.5, 0.01)
+        interp_y = np.arange(-7 + 0.1, 7, 0.04)
+
+        grid = (x, y, interp_x, interp_y)
+
+        self.run_tests(grid, label="Grid 1")
+
+
+    def test_grid_2(self):
+        print("Test grid 2:")
+        x = np.arange(-0.5, 0.5, 0.011)
+        y = np.arange(-7, 7, 0.11)
+        interp_x = np.arange(-0.5 + 0.02, 0.5, 0.011 / 2.01)
+        interp_y = np.arange(-7 + 0.1, 7, 0.11 / 2.01)
+
+        grid = (x, y, interp_x, interp_y)
+
+        self.run_tests(grid, label="Grid 2")
+        
+
+    def test_grid_3(self):
+        print("Test grid 3:")
+        x = np.arange(-0.5, 0.5, 0.011)
+        y = np.arange(-7, 7, 0.11)
+        interp_x = np.arange(-0.5 + 0.05, 0.5, 0.11)
+        interp_y = np.arange(-7 + 0.01, 7, 0.011)
+
+        grid = (x, y, interp_x, interp_y)
+
+        self.run_tests(grid, label="Grid 3")
         
         plt.show()
 