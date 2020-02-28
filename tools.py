8167cdc5df3b5af056b35aa2e4e088e9056ca48d
Filippo <filippotagliacarne@gmail.com>
2020-02-28 13:13:45 +0100

    Fixed some bugs

--- tools.py
+++ tools.py
@@ -1,5 +1,8 @@
 import numpy as np
 from aileronProperties import Aileron
+from math import cos, sin, sqrt, tan
+from test import  FiveIntegral, TripleIntegralZSC, DoubleIntegral, DoubleIntegralZSC, ThreeIntegral
+import sympy
 
 def macaulay(x,x1):
     return x-x1 if (x-x1)>0 else 0
@@ -8,10 +11,7 @@ def integrate1(f, a, b, n = 100, p = 1):
     X = np.linspace(a, b, num = n)
     h = (b-a)/(n-1)
     if p == 1:
-        result = np.sum(f(X))
-        result -= f(a)
-        result -= f(b)
-        result *= h
+        result = (np.sum(f(X))-0.5*f(a)-0.5*f(b))*h
     else:
         result = 0
         for i, _ in enumerate(X):
@@ -61,6 +61,8 @@ def solveInternal(alr: Aileron, q):
     x_3 = alr.x_3
     x_I = alr.x_I
     x_II = alr.x_II
+    x_a = alr.x_a
+    h_a = alr.h
     I_yy = alr.Iyy
     I_zz = alr.Izz
     J = 0.00024311681258111343
@@ -71,63 +73,101 @@ def solveInternal(alr: Aileron, q):
     r = alr.r
     d_1 = alr.d_1
     d_3 = alr.d_3
-    z_hat = -0.215
+    z_hat = 0.215
     P = alr.P
 
-    T = cos(theta)*r-sin(theta)*z_hat
+    T = sin(theta)*z_hat-cos(theta)*r
 
     def dtau(z, x):
         return q(z,x)*(z-z_hat)
 
+    '''
+    A = np.matrix([[x_1,1,0,0,z_hat+r,0,0,0,0,0,0,0],#v_1+p_1*(z+r) = d_1cos(theta)
+                   [0,0,x_1,1,r,0,0,0,0,0,0,0], #w_1-p_1*r = -d_1sin(theta)
+                   [x_2,1,0,0,z_hat+r,-(x_2-x_1)**3/(6*E*I_zz)-(z_hat+r)**2*(x_2-x_1)/(G*J),0,0,0,0,0,-sin(theta)*(x_2-x_I)**3/(6*E*I_zz)-T*(x_2-x_I)/(G*J)*(z_hat+r)],#v_2+p_2*(z+r) = 0
+                   [0,0,x_2,1,r,-r*(z_hat+r)*(x_2-x_1)/(G*J),(x_2-x_1)**3/6*E*I_yy,0,0,0,0,cos(theta)*(x_2-x_I)**3/(6*E*I_yy)+-r*T*(x_2-x_I)/(G*J)], #w_2-p_2*r = 0
+                   [x_3,1,0,0,z_hat+r,-(x_3-x_1)**3/(6*E*I_zz)-(z_hat+r)**2*(x_3-x_1)/(G*J),0,-(x_3-x_2)**3/(6*E*I_zz)-(z_hat+r)**2*(x_3-x_2)/(G*J),0,0,0,-sin(theta)*(x_3-x_I)**3/(6*E*I_zz)-T*(x_3-x_I)/(G*J)*(z_hat+r)], #v_3+p_3*(z+r) = d_3cos(theta)
+                   [0,0,x_3,1,r,-r*(z_hat+r)*(x_3-x_1)/(G*J),(x_3-x_1)**3/6*E*I_yy,-r*(z_hat+r)*(x_3-x_2)/(G*J),(x_3-x_2)**3/6*E*I_yy,0,0,cos(theta)*(x_3-x_I)**3/(6*E*I_yy)+-r*T*(x_3-x_I)/(G*J)], #w_3-p_3*r = -d_3sin(theta)
+                   [x_I*sin(theta),sin(theta),x_I*cos(theta),cos(theta),(z_hat+r)*sin(theta)-r*cos(theta),-sin(theta)*((x_I-x_1)**3/(6*E*I_zz)+(x_I-x_1)*(z_hat+r)**2/(G*J))+cos(theta)*r*(z_hat+r)/(G*J),cos(theta)*(x_I-x_1)**3/(6*E*I_yy),0,0,0,0,0],#sin(theta)*(v_I+p_I*(z+r))+cos(theta)*(w_I-p_I*r) = 0
+                   [0,0,0,0,0,1,0,1,0,1,0,sin(theta)], #S_z(l_a) = 0
+                   [0,0,0,0,0,0,-1,0,-1,0,-1,-cos(theta)], #S_y(l_a) = 0
+                   [0,0,0,0,0,l_a-x_1,0,l_a-x_2,0,l_a-x_3,0,sin(theta)*(l_a-x_I)], #M_z(l_a) = 0
+                   [0,0,0,0,0,0,-(l_a-x_1),0,-(l_a-x_2),0,-(l_a-x_3),-cos(theta)*(l_a-x_I)], #M_x(l_a) = 0
+                   [0,0,0,0,1,0,0,0,0,0,0,0]],dtype='float') #phi(0) = 0
+                   #[0,0,0,0,0,-(z_hat+r),0,-(z_hat+r),0,-(z_hat+r),0,-T]],dtype='float') #T(l_a) = 0
+
+    b = np.matrix([[d_1*cos(theta)-FiveIntegral(x_1)/(6*E*I_zz)-(z_hat+r)*TripleIntegralZSC(x_1,z_hat)/(G*J)],
+                   [-d_1*sin(theta)-r*TripleIntegralZSC(x_1,z_hat)/(G*J)],
+                   [0],
+                   [-r*TripleIntegralZSC(x_2, z_hat)/(G*J)],
+                   [d_3*cos(theta)-FiveIntegral(x_3)/(6*E*I_zz)-(z_hat+r)*TripleIntegralZSC(x_3,z_hat)/(G*J)-P*(sin(theta)*(x_3-x_II)**3/(6*E*I_zz)+T*(z_hat+r)*(x_3-x_II)/(G*J))],
+                   [-d_3*sin(theta)-r*TripleIntegralZSC(x_1,z_hat)/(G*J)+P*(cos(theta)*(x_3-x_II)**3/(6*E*I_yy)-r*T*(x_3-x_II)/(G*J))],
+                   [(-(z_hat+r)*sin(theta)+r*cos(theta))*TripleIntegralZSC(x_I,z_hat)/(G*J)-FiveIntegral(x_I)*sin(theta)/(6*E*I_zz)],
+                   [P*sin(theta)+DoubleIntegral(l_a)],
+                   [-P*cos(theta)],
+                   [+P*sin(theta)*(l_a-x_II)+ThreeIntegral(l_a)],
+                   [-P*cos(theta)*(l_a-x_II)],
+                   [0]],dtype='float')
+    '''
+    A = np.matrix([[x_1,1,0,0,z_hat+r,0,0,0,0,0,0,0],#v_1+p_1*(z+r) = d_1cos(theta)
+                   [0,0,-x_1,-1,-r,0,0,0,0,0,0,0], #w_1-p_1*r = -d_1sin(theta)
+                   [x_2,1,0,0,z_hat+r,-(x_2-x_1)**3/(6*E*I_zz)-(z_hat+r)**2*(x_2-x_1)/(G*J),0,0,0,0,0,-sin(theta)*(x_2-x_I)**3/(6*E*I_zz)-T*(x_2-x_I)/(G*J)*(z_hat+r)],#v_2+p_2*(z+r) = 0
+                   [0,0,-x_2,-1,-r,r*(z_hat+r)*(x_2-x_1)/(G*J),(x_2-x_1)**3/6*E*I_yy,0,0,0,0,cos(theta)*(x_2-x_I)**3/(6*E*I_yy)+r*T*(x_2-x_I)/(G*J)], #w_2-p_2*r = 0
+                   [x_3,1,0,0,z_hat+r,-(x_3-x_1)**3/(6*E*I_zz)-(z_hat+r)**2*(x_3-x_1)/(G*J),0,-(x_3-x_2)**3/(6*E*I_zz)-(z_hat+r)**2*(x_3-x_2)/(G*J),0,0,0,-sin(theta)*(x_3-x_I)**3/(6*E*I_zz)-T*(x_3-x_I)/(G*J)*(z_hat+r)], #v_3+p_3*(z+r) = d_3cos(theta)
+                   [0,0,x_3,1,-r,r*(z_hat+r)*(x_3-x_1)/(G*J),(x_3-x_1)**3/6*E*I_yy,r*(z_hat+r)*(x_3-x_2)/(G*J),(x_3-x_2)**3/6*E*I_yy,0,0,cos(theta)*(x_3-x_I)**3/(6*E*I_yy)+r*T*(x_3-x_I)/(G*J)], #w_3-p_3*r = -d_3sin(theta)
+                   [x_I*sin(theta),sin(theta),x_I*cos(theta),cos(theta),(z_hat+r)*sin(theta)-r*cos(theta),-sin(theta)*((x_I-x_1)**3/(6*E*I_zz)+(x_I-x_1)*(z_hat+r)**2/(G*J))+cos(theta)*r*(z_hat+r)/(G*J),cos(theta)*(x_I-x_1)**3/(6*E*I_yy),0,0,0,0,0],#sin(theta)*(v_I+p_I*(z+r))+cos(theta)*(w_I-p_I*r) = 0
+                   [0,0,0,0,0,1,0,1,0,1,0,sin(theta)], #S_z(l_a) = 0
+                   [0,0,0,0,0,0,-1,0,-1,0,-1,-cos(theta)], #S_y(l_a) = 0
+                   [0,0,0,0,0,l_a-x_1,0,l_a-x_2,0,l_a-x_3,0,sin(theta)*(l_a-x_I)], #M_z(l_a) = 0
+                   [0,0,0,0,0,0,-(l_a-x_1),0,-(l_a-x_2),0,-(l_a-x_3),-cos(theta)*(l_a-x_I)], #M_x(l_a) = 0
+                   [0,0,0,0,1,0,0,0,0,0,0,0]],dtype='float') #phi(0) = 0
+                   #[0,0,0,0,0,-(z_hat+r),0,-(z_hat+r),0,-(z_hat+r),0,-T]],dtype='float') #T(l_a) = 0
+
+
+    print(np.linalg.matrix_rank(A))
+    _, inds = sympy.Matrix(A).T.rref()
+    print(inds)
+
+    b = np.matrix([[d_1*cos(theta)-FiveIntegral(x_1)/(6*E*I_zz)-(z_hat+r)*TripleIntegralZSC(x_1,z_hat)/(G*J)],
+                   [-d_1*sin(theta)+r*TripleIntegralZSC(x_1,z_hat)/(G*J)],
+                   [-FiveIntegral(x_2)/(6*E*I_zz)-(z_hat+r)*TripleIntegralZSC(x_2,z_hat)/(G*J)],
+                   [r*TripleIntegralZSC(x_2, z_hat)/(G*J)],
+                   [d_3*cos(theta)-FiveIntegral(x_3)/(6*E*I_zz)-(z_hat+r)*TripleIntegralZSC(x_3,z_hat)/(G*J)-P*(sin(theta)*(x_3-x_II)**3/(6*E*I_zz)+T*(z_hat+r)*(x_3-x_II)/(G*J))],
+                   [-d_3*sin(theta)+r*TripleIntegralZSC(x_1,z_hat)/(G*J)+P*(cos(theta)*(x_3-x_II)**3/(6*E*I_yy)+r*T*(x_3-x_II)/(G*J))],
+                   [(-(z_hat+r)*sin(theta)+r*cos(theta))*TripleIntegralZSC(x_I,z_hat)/(G*J)-FiveIntegral(x_I)*sin(theta)/(6*E*I_zz)],
+                   [P*sin(theta)+DoubleIntegral(l_a)],
+                   [-P*cos(theta)],
+                   [+P*sin(theta)*(l_a-x_II)+ThreeIntegral(l_a)],
+                   [-P*cos(theta)*(l_a-x_II)],
+                   [0]],dtype='float')
+                   #[-P*T-DoubleIntegralZSC(l_a,z_hat)]],dtype='float')
+    #'''
+    '''
     A = np.matrix([[0,0,x_1,1,-r,0,0,0,0,0,0,0], #w(x_1) - p(x_1)*r = -d_1*sin(theta)
-                   [0,0,x_2,1,-r,0,((x_2-x_1)**3)/(6*E*I_yy),0,0,0,0,cos(theta)*((x_2-x_1)**3)/(6*E*I_yy)-T*r*(x_2-x_I)/(G*J)], #w(x_2) - p(x_2)*r = 0
-                   [0,0,x_3,1,-r,0,((x_3-x_1)**3)/(6*E*I_yy),0,((x_3-x_2)**3)/(6*E*I_yy),0,0,cos(theta)*((x_3-x_1)**3)/(6*E*I_yy)-T*r*(x_3-x_I)/(G*J)], #w(x_3) - p(x_3)*r = -d_3*sin(theta)
-                   [x_I*sin(theta),sin(theta),-x_I*cos(theta),-cos(theta),z_hat*sin(theta)-r*cos(theta),-((x_I-x_1)**3)*sin(theta)/(6*E*I_zz),((x_I-x_1)**3)*cos(theta)/(6*E*I_yy),0,0,0,0,0],
-                   [x_2,1,0,0,z_hat,-((x_2-x_1)**3)/(6*E*I_zz),0,0,0,0,0,-((x_2-x_I)**3)/(6*E*I_zz)*sin(theta)+z_hat*T*(x_2-x_I)/(G*J)], #v(x_2) + p(x_2)*z = 0
+                   [0,0,x_2,1,-r,r*(z_hat-r)*(x_2-x_1)/(G*J),((x_2-x_1)**3)/(6*E*I_yy),0,0,0,0,cos(theta)*((x_2-x_1)**3)/(6*E*I_yy)-T*r*(x_2-x_I)/(G*J)], #w(x_2) - p(x_2)*r = 0
+                   [0,0,x_3,1,-r,r*(z_hat-r)*(x_3-x_1)/(G*J),((x_3-x_1)**3)/(6*E*I_yy),r*(z_hat-r)*(x_3-x_2)/(G*J),((x_3-x_2)**3)/(6*E*I_yy),0,0,cos(theta)*((x_3-x_1)**3)/(6*E*I_yy)-T*r*(x_3-x_I)/(G*J)], #w(x_3) - p(x_3)*r = -d_3*sin(theta)
+                   [x_I*sin(theta),sin(theta),-x_I*cos(theta),-cos(theta),z_hat*sin(theta)-r*cos(theta),-((x_I-x_1)**3)*sin(theta)/(6*E*I_zz)-1/(G*J)*(z_hat-r)*(x_I-x_1)*(z_hat*sin(theta)-r*cos(theta)),((x_I-x_1)**3)*cos(theta)/(6*E*I_yy),0,0,0,0,0],
+                   [x_2,1,0,0,z_hat,-((x_2-x_1)**3)/(6*E*I_zz)-z_hat*(z_hat-r)*(x_2-x_1)/(G*J),0,0,0,0,0,-((x_2-x_I)**3)/(6*E*I_zz)*sin(theta)+z_hat*T*(x_2-x_I)/(G*J)], #v(x_2) + p(x_2)*z = 0
                    [x_1,1,0,0,z_hat,0,0,0,0,0,0,0], #v(x_1) + p(x_1)*z = d_1*cos(theta)
-                   [x_3,1,0,0,z_hat,-((x_3-x_1)**3)/(6*E*I_zz),0,-((x_3-x_2)**3)/(6*E*I_zz),0,0,0,-((x_3-x_I)**3)*sin(theta)/(6*E*I_zz)+T*z_hat*(x_3-x_I)/(G*J)], #v(x_3) + p(x_3)*z = d_3*cos(theta)
+                   [x_3,1,0,0,z_hat,-((x_3-x_1)**3)/(6*E*I_zz)-z_hat*(z_hat-r)*(x_3-x_1)/(G*J),0,-((x_3-x_2)**3)/(6*E*I_zz)-z_hat*(z_hat-r)*(x_3-x_2)/(G*J),0,0,0,-((x_3-x_I)**3)*sin(theta)/(6*E*I_zz)+T*z_hat*(x_3-x_I)/(G*J)], #v(x_3) + p(x_3)*z = d_3*cos(theta)
                    [0,0,0,0,0,0,-1,0,-1,0,-1,-cos(theta)], #S_y(l) = 0
                    [0,0,0,0,0,1,0,1,0,1,0,sin(theta)], #S_z(l) = 0
                    [0,0,0,0,0,0,x_1-l_a,0,x_2-l_a,0,x_3-l_a,(x_I-l_a)*sin(theta)], #M_y(l) = 0
                    [0,0,0,0,0,l_a-x_1,0,l_a-x_2,0,l_a-x_3,0,(l_a-x_I)*cos(theta)], #M_z(l) = 0
-                   [0,0,0,0,0,0,0,0,0,0,0,T]], dtype='float') #T(l) = 0
+                   [0,0,0,0,0,-(z_hat-r)*(l_a-x_1),0,-(z_hat-r)*(l_a-x_2),0,-(z_hat-r)*(l_a-x_3),0,T]], dtype='float') #T(l) = 0
 
-    b = np.matrix([[-d_1*sin(theta)-integrate2D(dtau,-C_a,0,0,x_1,10,10,p = 2)/(G*J)],
-                   [-integrate2D(dtau,-C_a,0,0,x_2,10,10,p = 2)/(G*J)],
-                   [-d_3*sin(theta)+P*(cos(theta)*((x_3-x_II)**3)/(2*E*I_yy)-T*r*(x_3-x_II))-integrate2D(dtau,-C_a,0,0,x_3,10,10,p=2)],
-                   [integrate2D(q,-C_a,0,0,x_I,10,10,p=4)*sin(theta)/(E*I_zz)-integrate2D(dtau,-C_a, 0, 0, l_a, 10, 10, p=2)*(z_hat*sin(theta)-r*cos(theta))],
-                   [-integrate2D(q,-C_a,0,0,x_2,10,10,p=4)/(E*I_zz)+integrate2D(dtau,-C_a,0,0,x_2,10,10,p=2)/(G*J)],
-                   [d_1*cos(theta)-integrate2D(q,-C_a,0,0,x_1,10,10,p=4)/(E*I_zz)+integrate2D(dtau,-C_a,0,0,x_1,10, 10, p=2)/(G*J)],
-                   [d_3*cos(theta)-integrate2D(q,-C_a,0,0,x_3,10,10,p=4)/(E*I_zz)-P*(sin(theta)*((x_3-x_II)**3)/(2*E*I_zz)-(x_3-x_II)/(G*J))+integrate2D(dtau,-C_a,0,0,x_3,10, 10, p=2)/(G*J)],
+    b = np.matrix([[-d_1*sin(theta)+TripleIntegralZSC(x_1,z_hat)/(G*J)],
+                   [+TripleIntegralZSC(x_2,z_hat)/(G*J)],
+                   [-d_3*sin(theta)+P*(cos(theta)*((x_3-x_II)**3)/(2*E*I_yy)-T*r*(x_3-x_II))+TripleIntegralZSC(x_3,z_hat)],
+                   [-FiveIntegral(x_I)*sin(theta)/(E*I_zz)+TripleIntegralZSC(l_a, z_hat)*(z_hat*sin(theta)-r*cos(theta))],
+                   [+FiveIntegral(x_2)/(E*I_zz)-TripleIntegralZSC(x_2,z_hat)/(G*J)],
+                   [d_1*cos(theta)+FiveIntegral(x_1)/(E*I_zz)-TripleIntegralZSC(x_1,z_hat)/(G*J)],
+                   [d_3*cos(theta)+FiveIntegral(x_3)/(E*I_zz)-P*(sin(theta)*((x_3-x_II)**3)/(2*E*I_zz)-(x_3-x_II)/(G*J))-TripleIntegralZSC(x_3,z_hat)/(G*J)],
                    [-P*cos(theta)],
-                   [integrate2D(q,-C_a,0,0,x_I,10,10,p=1)+P*sin(theta)],
+                   [-ThreeIntegral(l_a)+P*sin(theta)],
                    [P*(x_II-l_a)*cos(theta)],
-                   [integrate2D(q,-C_a,0,0,l_a,10,10,p=2)],
-                   [T*P+integrate2D(dtau,-C_a, 0, 0, l_a, 10, 10, p=1)]], dtype='float')
-    '''
-    A = np.matrix([[0,0,alr.x_1,1,0,0,0,0,0,0,0],
-                   [0,0,alr.x_2,1,0,((alr.x_2-alr.x_1)**3)/(6*alr.E*alr.Iyy),0,0,0,0,cos(alr.theta)*((alr.x_2-alr.x_1)**3)/(6*alr.E*alr.Iyy)],
-                   [0,0,alr.x_3,1,0,((alr.x_3-alr.x_1)**3)/(6*alr.E*alr.Iyy),0,((alr.x_3-alr.x_2)**3)/(6*alr.E*alr.Iyy),0,0,cos(alr.theta)*((alr.x_3-alr.x_1)**3)/(6*alr.E*alr.Iyy)],
-                   [-alr.x_I,-1,alr.x_I/tan(alr.theta),1/tan(alr.theta),((alr.x_I-alr.x_1)**3)/(6*alr.E*alr.Izz),-((alr.x_I-alr.x_1)**3)/(6*alr.E*alr.Iyy*tan(alr.theta)),0,0,0,0,0],
-                   [alr.x_2,1,0,0,-((alr.x_2-alr.x_1)**3)/(6*alr.E*alr.Izz),0,0,0,0,0,-((alr.x_2-alr.x_I)**3)/(6*alr.E*alr.Izz)*sin(alr.theta)],
-                   [alr.x_1,1,0,0,0,0,0,0,0,0,0],
-                   [alr.x_3,1,0,0,-((alr.x_3-alr.x_1)**3)/(6*alr.E*alr.Izz),0,-((alr.x_3-alr.x_2)**3)/(6*alr.E*alr.Izz),0,0,0,-((alr.x_3-alr.x_I)**3)*sin(alr.theta)/(6*alr.E*alr.Izz)],
-                   [0,0,0,0,0,-1,0,-1,0,-1,-cos(alr.theta)],
-                   [0,0,0,0,1,0,1,0,1,0,sin(alr.theta)],
-                   [0,0,0,0,0,alr.x_1-alr.l_a,0,alr.x_2-alr.l_a,0,alr.x_3-alr.l_a,(alr.x_I-alr.l_a)*sin(alr.theta)],
-                   [0,0,0,0,alr.l_a-alr.x_1,0,alr.l_a-alr.x_2,0,alr.l_a-alr.x_3,0,(alr.l_a-alr.x_I)*cos(alr.theta)]], dtype='float')
-
-    b = np.matrix([[-alr.d_1*sin(alr.theta)],
-                   [0],
-                   [-alr.d_3*sin(alr.theta)+alr.P*cos(alr.theta)*((alr.x_3-alr.x_II)**3)/(2*alr.E*alr.Iyy)],
-                   [integrate2D(q,-alr.C_a,0,0,alr.x_I,10,10,p=4)/(alr.E*alr.Izz)],
-                   [-integrate2D(q,-alr.C_a,0,0,alr.x_2,10,10,p=4)/(alr.E*alr.Izz)],
-                   [alr.d_1*cos(alr.theta)-integrate2D(q,-alr.C_a,0,0,alr.x_1,10,10,p=4)/(alr.E*alr.Izz)],
-                   [alr.d_3*cos(alr.theta)-integrate2D(q,-alr.C_a,0,0,alr.x_3,10,10,p=4)/(alr.E*alr.Izz)-alr.P*sin(alr.theta)*((alr.x_3-alr.x_II)**3)/(2*alr.E*alr.Izz)],
-                   [-alr.P*cos(alr.theta)],
-                   [integrate2D(q,-alr.C_a,0,0,alr.x_I,10,10,p=1)+alr.P*sin(alr.theta)],
-                   [alr.P*(alr.x_II-alr.l_a)*cos(alr.theta)],
-                   [integrate2D(q,-alr.C_a,0,0,alr.l_a,10,10,p=2)]], dtype='float')
+                   [-DoubleIntegral(l_a,)+P*(l_a-x_II)*sin(theta)],
+                   [T*P-DoubleIntegralZSC(l_a, z_hat)]], dtype='float')
     '''
     x = np.linalg.solve(A,b)
     header = 'C1, C2, C3, C4, C5, F_1y, F_1z, F_2y, F_2z, F_3y, F_3z, P_I'
