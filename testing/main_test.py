9317b646f9d64c16fa2a040362826fcdb47227cd
Benno <benno.riha@icloud.com>
2020-02-28 12:27:14 +0100

    Created testing folder and main_test file

---
+++ testing/main_test.py
@@ -0,0 +1,175 @@
+### Importing required packages
+import unittest
+from unittest import TestCase
+import numpy as np
+import Energy
+import Stiffness
+import Stress
+import math as m
+
+
+class GeometricalProperties(TestCase):
+    
+    def load_aircraft(self, name: str):
+        if name == "A320":
+            self.Ca    = 0.547
+            self.la    = 2.771
+            self.x1    = 0.153
+            self.x2    = 1.281
+            self.x3    = 2.681
+            self.xa    = 28.0
+            self.ha    = 22.5
+            self.tsk   = 1.1
+            self.tsp   = 2.9
+            self.tst   = 1.2
+            self.hst   = 1.5
+            self.wst   = 2.0
+            self.nst   = 17
+            self.d1    = 1.103
+            self.d3    = 1.642
+            self.theta = 26
+            self.P     = 91.7
+        elif name == "B737":
+            self.Ca    = 0.605
+            self.la    = 2.661
+            self.x1    = 0.172
+            self.x2    = 1.211
+            self.x3    = 2.591
+            self.xa    = 35.0
+            self.ha    = 20.5
+            self.tsk   = 1.1
+            self.tsp   = 2.8
+            self.tst   = 1.2
+            self.hst   = 1.6
+            self.wst   = 1.9
+            self.nst   = 15
+            self.d1    = 1.154
+            self.d3    = 1.840
+            self.theta = 28
+            self.P     = 97.4
+        elif name == "CRJ700":
+            self.Ca    = 0.484
+            self.la    = 1.691
+            self.x1    = 0.149
+            self.x2    = 0.554
+            self.x3    = 1.541
+            self.xa    = 27.2
+            self.ha    = 17.3
+            self.tsk   = 1.1
+            self.tsp   = 2.5
+            self.tst   = 1.2
+            self.hst   = 1.4
+            self.wst   = 1.8
+            self.nst   = 13
+            self.d1    = 0.681
+            self.d3    = 2.030
+            self.theta = 26
+            self.P     = 37.9
+        elif name == "Do228":
+            self.Ca    = 0.515
+            self.la    = 2.691
+            self.x1    = 0.174
+            self.x2    = 1.051
+            self.x3    = 2.512
+            self.xa    = 30.0
+            self.ha    = 24.8
+            self.tsk   = 1.1
+            self.tsp   = 2.2
+            self.tst   = 1.2
+            self.hst   = 1.5
+            self.wst   = 3.0
+            self.nst   = 11
+            self.d1    = 1.034
+            self.d3    = 2.066
+            self.theta = 25
+            self.P     = 20.6
+        elif name == "Fokker100":
+            self.Ca    = 0.505
+            self.la    = 1.611
+            self.x1    = 0.125
+            self.x2    = 0.498
+            self.x3    = 1.494
+            self.xa    = 24.5
+            self.ha    = 16.1
+            self.tsk   = 1.1
+            self.tsp   = 2.4
+            self.tst   = 1.2
+            self.hst   = 1.3
+            self.wst   = 1.7
+            self.nst   = 11
+            self.d1    = 0.389
+            self.d3    = 1.245
+            self.theta = 30
+            self.P     = 49.2
+            
+        
+        self.tsk /= 1e3  # mm to m
+        self.tsp /= 1e3  # mm to m
+        self.tst /= 1e3  # mm to m
+        self.hst /= 1e2  # cm to m
+        self.wst /= 1e2  # cm to m
+        self.d1  /= 1e2  # cm to m
+        self.d3  /= 1e2  # cm to m
+        self.theta = m.radians(self.theta)
+        self.P   *= 1e3  # kN to N
+        
+        ## Verification stuff
+        self.crosssection = Stiffness.Crosssection(nst,Ca,ha,tsk,tsp,tst,hst,wst)
+        crosssection.compute_bending_properties()   # Run the calculations
+        crosssection.compute_shearcenter()   # Run the calculations
+        crosssection.compute_torsionalstiffness()   # Run the calculations
+        
+        ## Model stuff
+        self.aileron(self.Ca, self.la, self.x1, self.x2, self.x3, self.xa, self.ha, self.tsk, self.tsp, self.tst, self.hst, self.wst, self.nst, self.d1, self.d3, self.theta, self.P)
+
+    
+    def test_geometry(self):
+        for aircraft in ["A320", "B737", "CRJ700", "Do228", "Fokker100"]:
+            print(f"Running test on {aircraft}")
+            self.load_aircraft(aircraft)
+            
+            print("- Stringer coordinates")
+            print("!!TODO: Compare automatically!!")
+            print(f"Should be: {self.crosssection.stcoord}")
+            print(f"       is: {self.aileron.stringersPosition()}")
+            
+            self.assertEqual(self.crosssection.stcoord, self.aileron.stringersPosition(), msg="Stringer positions are not correct.")
+            
+            print("- Cross-sectional area")
+            print(f"Should be: {self.crosssection.totarea}")
+            print(f"       is: {self.aileron.crossArea()}")
+            
+            self.assertEqual(self.crosssection.totarea, self.aileron.crossArea(), msg="Cross-sectional area is not correct.")
+            
+            print("- Centroid position")
+            print(f"Should be (y, z): {crosssection.yc, crosssection.zc}")
+            print(f"              is: {0, self.aileron.zCentroid}")
+            
+            self.assertEqual(crosssection.yc, 0, msg="Centroid y position not correct.")
+            self.assertEqual(crosssection.zc, self.aileron.zCentroid, msg="Centroid z position not correct.")
+            
+            print("- Moment of Inertia position")
+            print("!!TODO: Compare automatically!!")
+            print(f"Should be (I_yy, I_zz): {crosssection.Iyy, crosssection.Izz}")
+            print(f"                    is: {self.aileron.momInertia()}")
+            
+            self.assertEqual((crosssection.Iyy, crosssection.Izz), self.aileron.momInertia(), msg="MoI is not correct.")
+            
+            print("- Shear centre")
+            print("!!TODO: Compare automatically!!")
+            print(f"Should be (y, z): {crosssection.ysc, crosssection.zsc}")
+            print(f"              is: {'?', '?'}")
+            
+            self.assertEqual((crosssection.ysc, crosssection.zsc), (-1, -1), msg="Shear centre is not correct.")
+            
+            print("- Torsional constant")
+            print("!!TODO: Compare automatically!!")
+            print(f"Should be: {crosssection.J}")
+            print(f"         : {'?'}")
+            
+            self.assertEqual(crosssection.J, -1, msg="MoI is not correct.")
+        
+
+if __name__ == "__main__":
+    
+    unittest.main()
